e750a19fc35e90977e183649c54d0890475602f4
amiraadum <ava8264@nyu.edu>
2025-12-05 12:12:17 -0500

    Add paper trade option on market detail page, and tests; update styling

--- web_app/static/js/main.js
+++ web_app/static/js/main.js
@@ -1,2 +1,10 @@
 console.log("PolyPaper frontend loaded");
 
+document.addEventListener("DOMContentLoaded", () => {
+  const mockTradeBtn = document.querySelector(".trade-ticket .btn-primary");
+  if (mockTradeBtn) {
+    mockTradeBtn.addEventListener("click", () => {
+      alert("Trading will be enabled once the backend/API is connected.");
+    });
+  }
+});
